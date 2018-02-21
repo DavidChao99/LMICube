5caa7a7015c84b683b9689c62c86ac6a211d54c8
DavidChao99 <davidchao99@gmail.com>
2018-02-21 00:44:47 -0500

    Undo and turns done functionality added

--- app/src/main/java/com/example/david/lmicube/MyGLSurfaceView.java
+++ app/src/main/java/com/example/david/lmicube/MyGLSurfaceView.java
@@ -78,54 +78,54 @@ class MyGLSurfaceView extends GLSurfaceView {
                         if (abs(dy) / abs(dx) > 3 && dy < 0) {
                             // If on Left Face Right Column -> R
                             if (mPreviousX > .4239 * screenWidth && mPreviousX < .5475 * screenWidth) {
-                                mCube.Turn(1);
+                                mCube.Turn(1,true);
                             }
                             // If on Left Face Left Column -> L'
-                            else if (mPreviousX > .2787 * screenWidth && mPreviousX < .3758 * screenWidth) {
-                                mCube.Turn(4);
+                            else if (mPreviousX > .2787 * screenWidth && mPreviousX < .3558 * screenWidth) {
+                                mCube.Turn(4,true);
                             }
                             // If on Left Face Middle Column -> M'
-                            else if (mPreviousX > .3758 * screenWidth && mPreviousX < .4239 * screenWidth) {
-                                mCube.Turn(13);
+                            else if (mPreviousX > .3558 * screenWidth && mPreviousX < .4239 * screenWidth) {
+                                mCube.Turn(13,true);
                             }
                             // If on Right Face Left Column -> U'
                             else if (mPreviousX > .5525 * screenWidth && mPreviousX < .6275 * screenWidth) {
-                                mCube.Turn(6);
+                                mCube.Turn(6,true);
                             }
                             // If on Right Face Right Column -> D
                             else if (mPreviousX > .6688 * screenWidth && mPreviousX < .7688 * screenWidth) {
-                                mCube.Turn(9);
+                                mCube.Turn(9,true);
                             }
                             // If on Right Face Middle Column -> E
                             else if (mPreviousX > .6275 * screenWidth && mPreviousX < .6688 * screenWidth) {
-                                mCube.Turn(15);
+                                mCube.Turn(15,true);
                             }
                         }
                         // If swipe is vertical downwards
                         else if (abs(dy) / abs(dx) > 3 && dy > 0) {
                             // If on Left Face Right Column -> R'
                             if (mPreviousX > .4239 * screenWidth && mPreviousX < .5475 * screenWidth) {
-                                mCube.Turn(2);
+                                mCube.Turn(2,true);
                             }
                             // If on Left Face Left Column -> L
-                            else if (mPreviousX > .2787 * screenWidth && mPreviousX < .3758 * screenWidth) {
-                                mCube.Turn(3);
+                            else if (mPreviousX > .2787 * screenWidth && mPreviousX < .3558 * screenWidth) {
+                                mCube.Turn(3,true);
                             }
                             // If on Left Face Middle Column -> M
-                            else if (mPreviousX > .3758 * screenWidth && mPreviousX < .4239 * screenWidth) {
-                                mCube.Turn(14);
+                            else if (mPreviousX > .3558 * screenWidth && mPreviousX < .4239 * screenWidth) {
+                                mCube.Turn(14,true);
                             }
                             // If on Right Face Left Column -> U
                             else if (mPreviousX > .5525 * screenWidth && mPreviousX < .6275 * screenWidth) {
-                                mCube.Turn(5);
+                                mCube.Turn(5,true);
                             }
                             // If on Right Face Right Column -> D'
                             else if (mPreviousX > .6688 * screenWidth && mPreviousX < .7688 * screenWidth) {
-                                mCube.Turn(10);
+                                mCube.Turn(10,true);
                             }
                             // If on Right Face Middle Column -> E'
                             else if (mPreviousX > .6275 * screenWidth && mPreviousX < .6688 * screenWidth) {
-                                mCube.Turn(16);
+                                mCube.Turn(16,true);
                             }
                         }
 
@@ -135,30 +135,30 @@ class MyGLSurfaceView extends GLSurfaceView {
                             if (mPreviousX < .3758 * screenWidth) {
                                 // If on Top Row do B'
                                 if (mPreviousY < .3770 * screenHeight && mPreviousY > .2092 * screenHeight) {
-                                    mCube.Turn(12);
+                                    mCube.Turn(12,true);
                                 }
                                 // If on Bottom Row do F
                                 else if (mPreviousY < .7443 * screenHeight && mPreviousY > .5553 * screenHeight) {
-                                    mCube.Turn(7);
+                                    mCube.Turn(7,true);
                                 }
                                 // If on Middle Row do S
                                 else if (mPreviousY < .5553 * screenHeight && mPreviousY > .3770 * screenHeight) {
-                                    mCube.Turn(17);
+                                    mCube.Turn(17,true);
                                 }
                             }
                             //If on Right Face
                             else if (mPreviousX > .3758 * screenWidth) {
                                 // If on Top Row do B'
                                 if (mPreviousY < .5179 * screenHeight && mPreviousY > .3253 * screenHeight) {
-                                    mCube.Turn(12);
+                                    mCube.Turn(12,true);
                                 }
                                 // If on Bottom Row do F
                                 else if (mPreviousY < .9366 * screenHeight && mPreviousY > .7839 * screenHeight) {
-                                    mCube.Turn(7);
+                                    mCube.Turn(7,true);
                                 }
                                 // If on Middle Row do S
                                 else if (mPreviousY < .7839 * screenHeight && mPreviousY > .5179 * screenHeight) {
-                                    mCube.Turn(17);
+                                    mCube.Turn(17,true);
                                 }
                             }
                         }
@@ -169,30 +169,30 @@ class MyGLSurfaceView extends GLSurfaceView {
                             if (mPreviousX < .6688 * screenWidth) {
                                 // If on Top Row do B
                                 if (mPreviousY < .5179 * screenHeight && mPreviousY > .3253 * screenHeight) {
-                                    mCube.Turn(11);
+                                    mCube.Turn(11,true);
                                 }
                                 // If on Bottom Row do F'
                                 else if (mPreviousY < .9366 * screenHeight && mPreviousY > .7539 * screenHeight) {
-                                    mCube.Turn(8);
+                                    mCube.Turn(8,true);
                                 }
                                 // If on Middle Row do S'
                                 else if (mPreviousY < .7539 * screenHeight && mPreviousY > .5179 * screenHeight) {
-                                    mCube.Turn(18);
+                                    mCube.Turn(18,true);
                                 }
                             }
                             // If on Right Face
                             else if (mPreviousX > .6688 * screenWidth) {
                                 // If on Top Row do B
                                 if (mPreviousY < .3770 * screenHeight && mPreviousY > .2092 * screenHeight) {
-                                    mCube.Turn(11);
+                                    mCube.Turn(11,true);
                                 }
                                 // If on Bottom Row do F'
                                 else if (mPreviousY < .7443 * screenHeight && mPreviousY > .5253 * screenHeight) {
-                                    mCube.Turn(8);
+                                    mCube.Turn(8,true);
                                 }
                                 // If on Middle Row do S'
                                 else if (mPreviousY < .5253 * screenHeight && mPreviousY > .3770 * screenHeight) {
-                                    mCube.Turn(18);
+                                    mCube.Turn(18,true);
                                 }
                             }
                         }
@@ -206,7 +206,7 @@ class MyGLSurfaceView extends GLSurfaceView {
                     }
                     // Undo Button
                     else if( .26 * screenHeight < mPreviousY && mPreviousY < .49 * screenHeight ) {
-
+                        mCube.Undo();
                     }
                     // Reset Button
                     else if( .50 * screenHeight < mPreviousY && mPreviousY < .73 * screenHeight) {
--- app/src/main/java/com/example/david/lmicube/RubiksCube.java
+++ app/src/main/java/com/example/david/lmicube/RubiksCube.java
@@ -4,10 +4,16 @@ package com.example.david.lmicube;
  * Created by David on 8/16/2017.
  */
 
+import android.util.Log;
+
 import java.util.Random;
+import java.util.Stack;
+
+import static android.content.ContentValues.TAG;
 
 public class RubiksCube {
     private static int[][] cube;
+    private static Stack<Integer> turns;
 
     // WHITE - 0
     // GREEN - 1
@@ -18,6 +24,7 @@ public class RubiksCube {
 
     public RubiksCube() {
         cube = new int[6][9];
+        turns = new Stack<Integer>();
         Reset();
     }
 
@@ -39,8 +46,13 @@ public class RubiksCube {
 
     // These methods define Cube Logic
     // They return the Cube as a string but if this is unneeded, can remove and make void
-    public String Turn(int direction) {
+    public String Turn(int direction, boolean storesTurn) {
         String turn;
+        if(storesTurn) {
+            turns.push(Integer.valueOf(direction));
+            String pushmsg = String.format("We are pushing the turn %d", direction);
+            Log.i(TAG, pushmsg);
+        }
         int[] temp = new int[3];
         int temp1;
         switch (direction) {
@@ -75,14 +87,14 @@ public class RubiksCube {
 
                 break;
             case 2: turn = "R'";
-                Turn(1);
-                Turn(1);
-                Turn(1);
+                Turn(1, false);
+                Turn(1, false);
+                Turn(1, false);
                 break;
             case 3: turn = "L";
-                Turn(4);
-                Turn(4);
-                Turn(4);
+                Turn(4,false);
+                Turn(4,false);
+                Turn(4,false);
                 break;
             case 4: turn = "L'";
                 temp[0] = cube[0][0];
@@ -145,9 +157,9 @@ public class RubiksCube {
 
                 break;
             case 6: turn = "U'";
-                Turn(5);
-                Turn(5);
-                Turn(5);
+                Turn(5,false);
+                Turn(5,false);
+                Turn(5,false);
                 break;
             case 7: turn = "F";
                 temp[0] = cube[1][6];
@@ -180,9 +192,9 @@ public class RubiksCube {
 
                 break;
             case 8: turn = "F'";
-                Turn(7);
-                Turn(7);
-                Turn(7);
+                Turn(7,false);
+                Turn(7,false);
+                Turn(7,false);
                 break;
             case 9: turn = "D";
                 temp[0] = cube[0][6];
@@ -215,9 +227,9 @@ public class RubiksCube {
 
                 break;
             case 10: turn = "D'";
-                Turn(9);
-                Turn(9);
-                Turn(9);
+                Turn(9,false);
+                Turn(9,false);
+                Turn(9,false);
                 break;
             case 11: turn = "B";
                 temp[0] = cube[1][0];
@@ -250,9 +262,9 @@ public class RubiksCube {
 
                 break;
             case 12: turn = "B'";
-                Turn(11);
-                Turn(11);
-                Turn(11);
+                Turn(11,false);
+                Turn(11,false);
+                Turn(11,false);
                 break;
             // based upon ruwix.com/the-rubiks-cube/notation/ notation
             case 13: turn = "M";
@@ -274,14 +286,14 @@ public class RubiksCube {
 
                 break;
             case 14: turn = "M'";
-                Turn(13);
-                Turn(13);
-                Turn(13);
+                Turn(13,false);
+                Turn(13,false);
+                Turn(13,false);
                 break;
             case 15: turn = "E";
-                Turn(16);
-                Turn(16);
-                Turn(16);
+                Turn(16,false);
+                Turn(16,false);
+                Turn(16,false);
                 break;
             case 16: turn = "E'";
                 temp[0] = cube[0][3];
@@ -319,45 +331,45 @@ public class RubiksCube {
 
                 break;
             case 18: turn = "S'";
-                Turn(17);
-                Turn(17);
-                Turn(17);
+                Turn(17,false);
+                Turn(17,false);
+                Turn(17,false);
                 break;
             case 19: turn = "X";
                 // R L' M'
-                Turn(1);
-                Turn(4);
-                Turn(14);
+                Turn(1,false);
+                Turn(4,false);
+                Turn(14,false);
                 break;
             case 20: turn = "X'";
                 // R' L M
-                Turn(2);
-                Turn(3);
-                Turn(13);
+                Turn(2,false);
+                Turn(3,false);
+                Turn(13,false);
                 break;
             case 21: turn = "Y";
                 // U D' E'
-                Turn(5);
-                Turn(10);
-                Turn(16);
+                Turn(5,false);
+                Turn(10,false);
+                Turn(16,false);
                 break;
             case 22: turn = "Y'";
                 // U' D E
-                Turn(6);
-                Turn(9);
-                Turn(15);
+                Turn(6,false);
+                Turn(9,false);
+                Turn(15,false);
                 break;
             case 23: turn = "Z";
                 // F B' S
-                Turn(7);
-                Turn(12);
-                Turn(17);
+                Turn(7,false);
+                Turn(12,false);
+                Turn(17,false);
                 break;
             case 24: turn = "Z'";
                 // F' B S'x
-                Turn(8);
-                Turn(11);
-                Turn(18);
+                Turn(8,false);
+                Turn(11,false);
+                Turn(18,false);
                 break;
             default: turn = "Invalid";
                 break;
@@ -365,6 +377,23 @@ public class RubiksCube {
         }
 
         return getCube();
+    }
+
+    public String Undo() {
+        if(turns.empty()) {
+            return getCube();
+        }
+        else {
+            int dir = turns.pop();
+            String undomsg = String.format("We are undoing the turn %d", dir);
+            Log.i(TAG, undomsg);
+            if (dir % 2 == 0) {
+                this.Turn(dir - 1,false);
+            } else {
+                this.Turn(dir + 1,false);
+            }
+            return getCube();
+        }
     }
 
     public String Reset() {
@@ -382,7 +411,7 @@ public class RubiksCube {
         int numTurns = rand.nextInt(10) + 25;
         for(int i = 0; i <= numTurns; i++) {
             int turnNum = rand.nextInt(18) + 1;
-            Turn(turnNum);
+            Turn(turnNum,true);
         }
 
         return getCube();
